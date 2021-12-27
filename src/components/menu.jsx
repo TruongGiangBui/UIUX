29a2bfbb27f39a186794d8d662a6e674497a3064
truonggiangbui <truonggiangbui9@gmail.com>
2021-12-27 21:34:02 +0700

    init

---
+++ src/components/menu.jsx
@@ -0,0 +1,13 @@
+import "../css/menu.scss";
+import { Link } from "react-router-dom";
+const Menu = (props) => {
+
+  return (
+    <div className="menu">
+        <div className="app_name">
+            DX workplace
+        </div>
+    </div>
+  );
+};
+export default Menu;
