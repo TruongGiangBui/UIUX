3d2c8da814a3253ef7136fa5ba2cfdfc30e63d08
truonggiangbui <truonggiangbui9@gmail.com>
2022-01-13 15:09:31 +0700

    df

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -4,16 +4,7 @@ const Dashboard = (props) => {
 
   return (
     <div className="dashboard">
-     <div className="dropdown">
-  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown button
-  </button>
-  <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a className="dropdown-item" href="#">Action</a>
-    <a className="dropdown-item" href="#">Another action</a>
-    <a className="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
+
 
         <img src="https://uiux-xi.vercel.app/dashboard.png" alt="" srcset="" />
     </div>
